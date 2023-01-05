288cf0033b664cbe50f31d1f913fb4c4cddcdce7
Trang-Quynh <vitrang29@gmail.com>
2023-01-05 09:43:02 +0700

    add

---
+++ Bài tập javascript/Bai tap javascript bien kieu du lieu toan tu.js
@@ -0,0 +1,8 @@
+let diemvatly
+let diemhoahoc
+let diemsinhhoc
+diemvatly = parseInt(prompt("nhập điểm vật lý"));
+diemhoahoc = parseInt(prompt("nhập điểm hóa học"));
+diemsinhhoc = parseInt(prompt("nhập điểm sinh học"));
+let diemtrungbinh= (diemvatly+diemhoahoc+diemsinhhoc)/3
+document.write("Điểm trung bình 3 môn là: " + diemtrungbinh)
